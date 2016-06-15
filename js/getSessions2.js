7793df0f08acf26dd16fc111d5f5b655735df5a5
Willy Karam <admin@willykaram.com>
2016-06-15 04:14:22 -0400

    initial session code

---
+++ js/getSessions2.js
@@ -0,0 +1,44 @@
+function getSessions(sessions) {
+
+//  var sessions = sessions.filter(sessionsFilter);
+
+  var outputSessions = "";
+  
+  var i;
+	  for(i = 0; i<sessions.length; i++) {
+		  outputSessions +=
+				'<div class="session">'+
+					'<div class="session-inner">'+ 
+						'<div class="camp">' + sessions[i].camp + '</div>'+
+						'<div class="slot">' + sessions[i].timeStart + ' to ' + sessions[i].timeStop + '</div>'+
+						'<div class="icon"> <img src="images/sessions/icons' + sessions[i].icon + '" class="icon"/></div>'+
+						'<div class="title"><h4>' + sessions[i].sessionTitle + '</h4></div>'+
+						'<div class="summary"><p class="summary">' + sessions[i].sessionSummary + '</p></div>'+
+						'<div class="speakers">'+
+							'<div class="speaker">'+
+								'<div class="speaker-inner">'+
+									'<div class="photo"><img src="images/speakers/' + sessions[i].speakers.speaker[0].photo + '"></div>'+
+									'<div class="details">'+
+										'<div class="name"><h5 class="speaker-name">' + sessions[i].speakers.speaker[0].nameFirst + ' ' + sessions[i].speakers.speaker[0].nameLast + '</h5></div>'+
+				    				'<div class="organization">' + sessions[i].speakers.speaker[0].organization + '</div>'+
+						    		'<div class="role">' + sessions[i].speakers.speaker[0].role + '</div>'+
+								    '<div class="links">'+
+											'<div class="link linkedin"><a href="' + sessions[i].speakers.speaker[0].linkedin  + '" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i></a></div>'+    
+		    							'<div class="link twitter"><a href="' + sessions[i].speakers.speaker[0].twitter + '" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></div>'+    
+				    				  '<div class="link github"><a href="' + sessions[i].speakers.speaker[0].github + '" target="_blank" class="twitter"><i class="fa fa-github"></i></a></div>'+    
+				    				  '<div class="link website"><a href="' + sessions[i].speakers.speaker[0].website + '" target="_blank" class="website"><i class="fa fa-globe"></i></a></div>'+
+								    '</div>'+
+        					'</div>'+
+      					'</div>'+
+      				'</div>'+
+					  '</div>'+
+						'<div class="slides">' + sessions[i].sessionSlides + '</div>'+
+						'<div class="video">'+
+  						'<h5 class="video">Video</h5>'+
+  						'<div class="embed">' + sessions[i].sessionVideo + '</div>'+
+						'</div>'+
+					'</div>'+
+        '</div>';
+    }
+  document.getElementById("sessions2").innerHTML = outputSessions;
+}
---
+++ js/listSessions.js
@@ -0,0 +1,396 @@
+﻿getSessions([
+
+  // APPS CAMP
+	{
+		"camp":"Apps Camp",
+		"tech":"apps",
+		"date":"2016-07-09",
+		"timeStart":"11:30",
+		"timeStop":"12:15",
+		"sessionTitle":"Real Time",
+		"sessionSubTitle":"",
+		"sessionSummary":"",
+		"sessionType":"keynote",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Phil",
+				  "nameLast":"Leggetter",
+				  "photo":"speaker-phil-leggetter.jpg",
+				  "linkedin":"https://www.linkedin.com/in/leggetter",
+				  "twitter":"http://twitter.com/aleaabed",
+				  "github":"",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"Nexmo",
+				  "role":"Head of Developer Relations",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+
+	{
+		"camp":"Apps Camp",
+		"teach":"apps",
+		"date":"2016-07-09",
+		"timeStart":"13:30",
+		"timeStop":"14:15",
+		"sessionTitle":"Horizon JS",
+		"sessionSubTitle":"",		
+		"sessionSummary":"",
+		"sessionType":"keynote",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Mike ",
+				  "nameLast":"Glukhovsky",
+				  "photo":"speaker-mike-glukhovsky.jpg",
+				  "linkedin":"https://www.linkedin.com/in/mglukhovsky",
+				  "twitter":"http://twitter.com/mglukhovsky",
+				  "github":"https://github.com/mglukhovsky",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"RethinkDB",
+				  "role":"Founder",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+
+	{
+		"camp":"Apps Camp",
+		"teach":"apps",
+		"date":"2016-07-09",
+		"timeStart":"13:30",
+		"timeStop":"14:15",
+		"sessionTitle":"Firebase for Fun & Profit: Mobile, Responsivee & Cross-Platform App Development",
+		"sessionSubTitle":"",		
+		"sessionSummary":"",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Nitya",
+				  "nameLast":"Narasimhan",
+				  "photo":"speaker-nitya-narasimhan.jpg",
+				  "linkedin":"https://www.linkedin.com/in/nityan",
+				  "twitter":"https://twitter.com/nitya",
+				  "github":"https://github.com/nitya",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"Digital HV",
+				  "role":"Owner",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+
+	{
+		"camp":"Apps Camp",
+		"teach":"apps",
+		"date":"2016-07-09",
+		"timeStart":"13:30",
+		"timeStop":"14:15",
+		"sessionTitle":"So, You've Decided On 'API First'",
+		"sessionSubTitle":"",		
+		"sessionSummary":"Building your next mobile app, SaaS product, or killer social network using an API first philosophy is a great decision. It can allow your team to develop in parallel, enable expansion to multiple platforms, and create opportunity for outside integration. But once you've made that decision, what's next? How do you actually do it? /n What options are there for authentication? How do you reset a password with an API? Is hypermedia worth considering for internal APIs? How do you handle unauthenticated access from a client side javascript application? What format is best for API requests and responses? Should you version your API now, or later? Let's talk about how you can determine the right answers for all those questions, and give your API a solid foundation. Because your API may start out serving the needs of internal mobile clients, or single page web apps, but one day (sooner than you think) you'll probably be turning it into a public API.",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Tim",
+				  "nameLast":"Lytle",
+				  "photo":"speaker-tim-lytle.jpg",
+				  "linkedin":"https://www.linkedin.com/in/timlytle",
+				  "twitter":"https://twitter.com/tjlytle",
+				  "github":"https://github.com/tjlytle",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"Nexmo",
+				  "role":"Developer Evangelist",
+				  "bio":"As a seasoned contract developer Tim's built applications utilizing numerous APIs, created custom APIs for internal consumption, and now helps other developers energize their projects with Nexmo's powerful global communications API. A lover APIs, he also hates the top reply, and tweets sporadically from @tjlytle. "
+        }
+      ]
+    }  
+	},
+
+
+
+
+  // FRONTEND CAMP
+	{
+		"camp":"Front End Camp",
+		"tech":"frontend",
+		"date":"2016-07-17",
+		"timeStart":"TBA",
+		"timeStop":"TBA",
+		"sessionTitle":"The Frontend for 2016 (and not 2013)",
+		"sessionSubTitle":"",
+		"sessionSummary":"2013 saw the term MEAN (MongoDB, Express.js, Angular.js, Node.js) enter the tech lexicon. This JavaScript-driven stack was sometimes sought as a means to abandon the more traditional LAMP stack. Even if LAMP and similar backends were retained, there was a general move toward decoupling the frontend from the backend. Things have changed quickly in 3 years. We'll discuss new technologies, as well as old, as well as common pitfalls encountered in frontend development, both past and present, and discuss solutions to overcome them.",
+		"sessionType":"session",
+		"sessionSlides":"TBA",
+		"sessionVideo":"TBA",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Hyunho",
+				  "nameLast":"Ee",
+				  "photo":"speaker-hyunho-ee.jpg",
+				  "linkedin":"",
+				  "twitter":"",
+				  "github":"https://github.com/e2tha-e",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"RethinkDB",
+				  "role":"Software Engineer",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+
+
+
+
+  // FRONTEND CAMP
+	{
+		"camp":"Phoenix Camp NYC",
+		"tech":"phoenix",
+		"date":"2016-07-16",
+		"timeStart":"14:30",
+		"timeStop":"15:30",
+		"sessionTitle":"From Rails to Phoenix",
+		"sessionSubTitle":"",
+		"sessionSummary":"Many Rails developers are jumping ship for Phoenix, learn why you and your organization should consider making the same move. Case studies and hard data will tell the story of how Phoenix is here to stay but also the best choice for new and existing applications.",
+		"sessionType":"session",
+		"sessionSlides":"<script async class=\"speakerdeck-embed\" data-id=\"6ba495d8923c4a89a6de7a5ea4443686\" data-ratio=\"1.33333333333333\" src=\"//speakerdeck.com/assets/embed.js\"></script>",
+		"sessionVideo":"<iframe width=\"480\" height=\"315\" src=\"https://www.youtube.com/embed/530z-_yjdlU\" frameborder=\"0\" allowfullscreen></iframe>",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Brian",
+				  "nameLast":"Cardarella",
+				  "photo":"speaker-brian-cardarella.jpg",
+				  "linkedin":"https://www.linkedin.com/in/briancardarella",
+				  "twitter":"https://twitter.com/bcardarella",
+				  "github":"https://github.com/bcardarella",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"Dockyard",
+				  "role":"Founder, CEO",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+	
+	{
+		"camp":"Phoenix Camp NYC",
+		"tech":"phoenix",
+		"date":"2016-07-16",
+		"timeStart":"TBA",
+		"timeStop":"TBA",
+		"sessionTitle":"Building & Deploying a Real Suite of OTP Apps",
+		"sessionSubTitle":"",
+		"sessionSummary":"An examination of Phoenix, OTP, Umbrellas, and more in Elixir in Phoenix from 0 to deployment. Recently we had to build a large suite of services for a client and this examines the net wins on both the engineering and business sides of the equation; diving into architecting and building OTP apps, Phoenix, Ecto, and deploying it all into the real world.",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Bobby",
+				  "nameLast":"Grayson",
+				  "photo":"speaker-bobby-grayson.jpg",
+				  "linkedin":"https://www.linkedin.com/in/robert-grayson-92506872",
+				  "twitter":"https://twitter.com/yburyug",
+				  "github":"https://github.com/ybur-yug",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"CometaWorks",
+				  "role":"Software Engineer - Backend, Systems, API, ETL",
+				  "bio":"Bobby is a wannabe comic, nomad, and sailor living in San Juan, Puerto Rico building cool things in Elixir, Ruby, Phoenix, and Ember.js."
+        }
+      ]
+    }  
+	},
+
+
+
+
+  // FRONTEND CAMP
+	{
+		"camp":"React Camp",
+		"tech":"react",
+		"date":"2016-07-16",
+		"timeStart":"TBA",
+		"timeStop":"TBA",
+		"sessionTitle":"React-Fire: Getting Full-Stack reactivity with Firebase",
+		"sessionSubTitle":"",
+		"sessionSummary":"",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Nitya",
+				  "nameLast":"Narasimhan",
+				  "photo":"speaker-nitya-narasimhan.jpg",
+				  "linkedin":"https://www.linkedin.com/in/nityan",
+				  "twitter":"https://twitter.com/nitya",
+				  "github":"https://github.com/nitya",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"Digital HV",
+				  "role":"Owner",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+
+
+
+
+  //REACT
+
+	{
+		"camp":"React Camp",
+		"tech":"react",
+		"date":"2016-07-16",
+		"timeStart":"",
+		"timeStop":"",
+		"sessionTitle":"Comprehensive Navigation in React Native",
+		"sessionSubTitle":"",
+		"sessionSummary":"There are multiple available options for navigation in React Native, we will discuss and demo the current and future platform apis for navigation, as well as a couple of community projects.",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Nader",
+				  "nameLast":"Dabit",
+				  "photo":"speaker-nader-dabit.jpg",
+				  "linkedin":"https://www.linkedin.com/in/naderdabit",
+				  "twitter":"https://twitter.com/dabit3",
+				  "github":"https://github.com/dabit3",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"School Status",
+				  "role":"Software Developer",
+				  "bio":"Nader Dabit has been developing using React Native at the company which he is employed, SchoolStatus, since the framework's release, and has also worked with React Native on multiple freelance and consulting projects over the past year. Nader has also been active in the React Native community, mainly on StackOverflow, Medium, and with the React Native Radio podcast on Devchat.tv. He founded and organizes two local tech meetups and is the cofounder of Mississippi's first coding school, Code South Labs. He has been creating curriculum as well as teaching JavaScript and JavaScript frameworks / technologies (Node, Angular, React, and React Native). Nader is also currently authoring React Native in Action for Manning Publications."
+        }
+      ]
+    }  
+	},
+	
+	{
+		"camp":"React Camp",
+		"tech":"react",
+		"date":"2016-07-16",
+		"timeStart":"",
+		"timeStop":"",
+		"sessionTitle":"Redux with React Native",
+		"sessionSubTitle":"",
+		"sessionSummary":"Introduction to Redux with React Native with an open source code example",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Dan",
+				  "nameLast":"Jensen",
+				  "photo":"speaker-dan-jensen.jpg",
+				  "linkedin":"https://www.linkedin.com/in/danjensen",
+				  "twitter":"",
+				  "github":"https://github.com/urbanvikingr",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"",
+				  "role":"Mentor and Freelancer in React, Node.js, Ruby on Rails and Agile Leadership",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+
+	{
+		"camp":"React Camp",
+		"tech":"react",
+		"date":"2016-07-16",
+		"timeStart":"TBA",
+		"timeStop":"TBA",
+		"sessionTitle":"Why is React 'functional?'",
+		"sessionSubTitle":"",
+		"sessionSummary":"We all know that React works, but being functional implies so much more than the typical dictionary definition would imply. Using React  as an example, I’d like to walk you through some tenets of functional programming. As one of the few JavaScript view creation libraries that is completely Universal/Isomorphic, React gives us the opportunity to build applications that can run on the server and client very easily. Typically though, we are discussing the 'virtual DOM' and performance when talking about React. A win much bigger than the words ‘virtual DOM’ exists in React, and through my examples, you’ll see what makes reasoning about functional systems such a treat.",
+		"sessionType":"session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"John K.",
+				  "nameLast":" Paul",
+				  "photo":"speaker-john-paul.jpg",
+				  "linkedin":"https://www.linkedin.com/in/johnkpaul",
+				  "twitter":"https://twitter.com/johnkpaul",
+				  "github":"https://github.com/johnkpaul",
+				  "facebook":"",
+				  "website":"http://www.johnkpaul.com/",
+				  "organization":"InRythm",
+				  "role":"CTO",
+				  "bio":""
+        }
+      ]
+    }  
+	},
+	
+	{
+		"camp":"React Camp",
+		"tech":"react",
+		"date":"2016-07-16",
+		"timeStart":"TBA",
+		"timeStop":"TBA",
+		"sessionTitle":"Building React Native Apps with Deco",
+		"sessionSubTitle":"",
+		"sessionSummary":"Showing off Deco, the IDE built for React Native",
+		"sessionType":"lightning",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    "speakers": {
+      "speaker": [
+			  {
+ 				  "nameFirst":"Timotius",
+				  "nameLast":"Sitorus",
+				  "photo":"speaker-timotius-sitorus.jpg",
+				  "linkedin":"https://www.linkedin.com/in/timotius",
+				  "twitter":"https://twitter.com/timsitorus",
+				  "github":"https://github.com/timotius02",
+				  "facebook":"",
+				  "website":"",
+				  "organization":"NYHackathons",
+				  "role":"Designer, Developer, and Hackathon Hacker. Web Developer at NYHackathons.com.",
+				  "bio":""
+        }
+      ]
+    }  
+	}
+
+
+])
+
+
