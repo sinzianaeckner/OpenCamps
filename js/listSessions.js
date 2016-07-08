2c818f70e67208b2e3614da7345384a5c10b9b67
Christie Ewen <christieewen@users.noreply.github.com>
2016-07-08 00:32:13 -0400

    Update listSessions.js
    Speaker Bud Mishra at AI Camp on July 12, 2016 at 2:45pm-3:45pm

--- js/listSessions.js
+++ js/listSessions.js
@@ -18,6 +18,65 @@
 		"camp":"AI Camp",
 		"tech":"ai",
 		"date":"2016-07-12",
+		"timeStart":"14:45",
+		"timeStop":"15:45",
+		"sessionTitle":"AI: Enigma of Imitation",
+		"sessionSubTitle":"",
+		"sessionSummary":"<p>"Talk will be about how Turing developed the ideas of AI in terms of an imitation game, but failed to address the question of how imitative intelligence may affect the human societies, their norms and institutions. Some ideas from information-asymmetric (signaling) games will be used to explore how we could think about the future of AI and human."</p>",
+		"sessionType":"Session",
+		"sessionSlides":"",
+		"sessionVideo":"",
+    		"speakers": {
+      		"speaker": [
+			  {
+ 				  "nameFirst":"Bud",
+				  "nameLast":"Mishra",
+				  "photo":"speaker-bud-mishra.jpg",
+				  "linkedin":"https://www.linkedin.com/in/budmishra",
+				  "twitter":"https://twitter.com/bud_mishra",
+				  "github":"",
+				  "facebook":"",
+				  "website":"https://cs.nyu.edu/mishra/",
+				  "openSourceRole":"",
+				  "organization":"NYU",
+				  "role":"Professor",
+				  "bio":"<p>Professor Bud Mishra is a professor of computer science and mathematics at 
+				  NYU Courant Institute of Mathematical Sciences, professor of engineering at NYU’s Tandon School 
+				  of engineering, professor of human genetics at Mt. Sinai School of Medicine, visiting scholar at 
+				  Cold Spring Harbor Laboratory and a professor of cell biology at NYU School of Medicine. 
+				  He founded the NYU/Courant Bioinformatics Group, a multi-disciplinary group working on research 
+				  at the interface of computer science, applied mathematics, biology, biomedicine and 
+				  bio/nano-technologies. Prof. Mishra has a degree in Science from Utkal University, in 
+				  Electronics and Communication Engineering from IIT, Kharagpur, and MS and PhD degrees in 
+				  Computer Science from Carnegie-Mellon University. He has industrial experience in 
+				  Computer and Data Science (ATTAP, brainiad, Genesis Media, Pypestream, and Tartan Laboratories), 
+				  Finance (Instadat, PRF, LLC, and Tudor Investment), Robotics and Bio- and 
+				  Nanotechnologies (Abraxis, Bioarrays, InSilico, MRTech, OpGen and Seqster). 
+				  He is the author of a textbook on algorithmic algebra and more than two hundred archived publications.
+				  He has advised and mentored more than 35 graduate students and post-docs in the areas of computer 
+				  science, robotics and control engineering, applied mathematics, finance, biology and medicine. 
+				  He holds 21 issued and 23 pending patents in areas
+				  ranging over robotics, model checking, intrusion detection, cyber security,
+				  emergency response, disaster management, data analysis, biotechnology,
+				  nanotechnology, genome mapping and sequencing, mutation calling, cancer
+				  biology, fintech, adtech, internet architecture and linguistics. His pioneering work
+				  includes: first application of model checking to hardware verification; first robotics
+				  technologies for grasping, reactive grippers and work holding; first single molecule
+				  genotype/haplotype mapping technology (Optical Mapping); first analysis of copy
+				  number variants with a segmentation algorithm, first whole-genome haplotype
+				  assembly technology (SUTTA), first clinical-genomic variant/base calling
+				  technology (TotalRecaller), and current work in progress continuing in the areas of
+				  liquid biopsies, cancer data, cyber security, cryptography, financial engineering and
+				  internet of the future. He is a fellow of IEEE, ACM and AAAS, a Distinguished
+				  Alumnus of IIT-Kgp, and a NYSTAR Distinguished Professor.</p>",
+        }
+      ]
+    }  
+	},
+    {
+		"camp":"AI Camp",
+		"tech":"ai",
+		"date":"2016-07-12",
 		"timeStart":"09:15",
 		"timeStop":"09:55",
 		"sessionTitle":"Keynote",
